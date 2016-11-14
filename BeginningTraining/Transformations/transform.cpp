c86f55783b907f62769e63b0c2a2284740ca627b
CutieOwl <kml11@live.com>
2016-11-13 20:02:37 -0800

    reflection addition to Transformations problem program

--- BeginningTraining/Transformations/transform.cpp
+++ BeginningTraining/Transformations/transform.cpp
@@ -24,12 +24,43 @@ bool areSame(const vector<string> &before, const vector<string> &after) {
 	return same;
 }
 
+void rotate90(const vector<string> &before, vector<string> &output) {
+
+}
+
+void reverseString(const string &input, string &s) {
+	int size = input.size();
+	for (int i = 0; i < size; ++i) {
+		s[size - i - 1] = input[i];
+	}
+}
+
+void reflectStrings(const vector<string> &before, vector<string> &output) {
+	int n = (int)before.size();
+	for (int i = 0; i < n; ++i) {
+		string s(before[i].size(), '@');
+		reverseString(before[i], s);
+		output[i] = s;
+	}
+}
+
 int detectChange(const vector<string> &before, const vector<string> &after) {
+	int n = (int)before.size();
+	vector<string> output1(n); 
+	vector<string> output2(n);
+	vector<string> output3(n);
+	vector<string> output4(n);
 	int retVal = 7;
 	
 	// check if both vectors are the same (no change, #6)
 	if (areSame(before, after)) {
 		retVal = 6;
+	}
+
+	// check if a reflection occured 
+	reflectStrings(before, output4);
+	if (areSame(output4, after)) {
+		retVal = 4;
 	}
 
 	return retVal;
