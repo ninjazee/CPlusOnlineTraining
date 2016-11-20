1b85cf5fcec0ce386755c774f20af0bf4b04f5ba
CutieOwl <kml11@live.com>
2016-11-19 21:57:17 -0800

    Bull in China Shop

---
+++ BeginningTraining/BullInAChinaShop/bcs.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+int numTiles(const vector<string> &figure) {
+	int n = (int)figure.size();
+	int count = 0;
+	for (int a = 0; a < n; ++a) {
+		for (int b = 0; b < n; ++b) {
+			if (figure[a][b] == '#') {
+				count += 1;
+			}
+		}
+	}
+	return count;
+}
+
+int main() {
+	ofstream fout("bcs.out");
+	ifstream fin("bcs.in");
+	int n, k;
+	fin >> n >> k;
+	vector<string> originalFig(n);
+	for (int i = 0; i < n; ++i) {
+		string line;
+		fin >> line;
+		originalFig[i] = line;
+	}
+	vector<vector<string>> pieces(k);
+	for (int j = 0; j < k; ++j) {
+		vector<string> piece(n);
+		for (int h = 0; h < n; ++h) {
+			string line;
+			fin >> line;
+			piece[h] = line;
+		}
+		pieces[j] = piece;
+	}
+	int originalSize = numTiles(originalFig);
+	vector<int> pieceSizes(k);
+	for (int z = 0; z < k; ++z) {
+		int size = numTiles(pieces[z]);
+		pieceSizes[z] = size;
+	}
+
+	for (int y = 0; y < k-1; ++y) {
+		for (int x = y+1; x < k; ++x) {
+			if (pieceSizes[x] + pieceSizes[y] == originalSize) {
+				fout << y+1 << ' ' << x+1 << endl;
+				return 0;
+			}
+		}
+	}
+
+	
+}
