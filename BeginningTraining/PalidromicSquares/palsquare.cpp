4fe98950f3f2fef89c69a6493f244f74d02d0399
CutieOwl <kml11@live.com>
2017-01-01 21:32:53 -0800

    Palindromic Squares Rob Kolstad Palindromes are numbers that read the same forwards as backwards. The number 12321 is a typical palindrome. Given a number base B (2 <= B <= 20 base 10), print all the integers N (1 <= N <= 300 base 10) such that the square of N is palindromic when expressed in base B; also print the value of that palindromic square. Use the letters 'A', 'B', and so on to represent the digits 10, 11, and so on. Print both the number and its square in base B. PROGRAM NAME: palsquare INPUT FORMAT A single line with B, the base (specified in base 10). SAMPLE INPUT (file palsquare.in) 10 OUTPUT FORMAT Lines with two integers represented in base B. The first integer is the number whose square is palindromic; the second integer is the square itself. NOTE WELL THAT BOTH INTEGERS ARE IN BASE B! SAMPLE OUTPUT (file palsquare.out) 1 1 2 4 3 9 11 121 22 484 26 676 101 10201 111 12321 121 14641 202 40804 212 44944 264 69696

--- BeginningTraining/PalidromicSquares/palsquare.cpp
+++ BeginningTraining/PalidromicSquares/palsquare.cpp
@@ -1,6 +1,6 @@
 /*
 ID: kml111
-PROG: ride
+PROG: palsquare
 LANG: C++11
 */
 #include <iostream>
@@ -16,20 +16,38 @@ LANG: C++11
 
 using namespace std;
 
+void reverseVectorChangeToString(const vector<char> &start, string &end) {
+	end.resize((int)start.size(), '0');
+	for (int c = 0; c < (int)start.size(); ++c) { // for each char in the vector<char> start
+		end[c] = start[(int)start.size() - c - 1]; // add to string putting the chars in the back in the front
+	}
+}
+
+void intToStringBase(const int n, string &ret, const int b) {
+	vector<char> storage; // create empty storage vector
+	int i = n;
+	while (i / b != 0) {
+		char c = (i % b > 9 ? i % b - 10 + 'A' : i % b + '0'); // if n % b > 9 then make c the correct letter, else make it the correct number
+		storage.push_back(c); // add the character to the storage
+		i = i / b; // repeat with n / b in place of n
+	} 
+	char c = (i % b > 9 ? i % b - 10 + 'A' : i % b + '0'); // add the last digit
+	storage.push_back(c); // push it in
+	reverseVectorChangeToString(storage, ret); // reverse it and change to string
+}
+
 void palindromicSquares(const int b, vector<pair<string, string>> &psquares) {
 	for (int n = 1; n <= 300; ++n) { // go through every integer in base 10 from 1 to 300
 		// write down square of integer
 		int sq = n * n;
 
-		// convert the integer and its square to base b
-		char storagen [33];
-		itoa(n, storagen, b);
-		char storagesq[33];
-		itoa(sq, storagesq, b);
+		// create strings for the numbers and its squares in the base
+		string bn;
+		string bsq;
 
-		// convert char array to string
-		string bn(storagen);
-		string bsq(storagesq);
+		// convert the integer and its square to base b
+		intToStringBase(n, bn, b);
+		intToStringBase(sq, bsq, b);
 
 		// check if the square of bn is a palindrome
 		if ((int)bsq.length() == 1) {
