2722fcd797b797536afa0998d885e232f4aef3e0
CutieOwl <kml11@live.com>
2017-02-04 20:00:34 -0800

    Wormholes Farmer John's hobby of conducting high-energy physics experiments on weekends has backfired, causing N wormholes (2 <= N <= 12, N even) to materialize on his farm, each located at a distinct point on the 2D map of his farm (the x,y coordinates are both integers). According to his calculations, Farmer John knows that his wormholes will form N/2 connected pairs. For example, if wormholes A and B are connected as a pair, then any object entering wormhole A will exit wormhole B moving in the same direction, and any object entering wormhole B will similarly exit from wormhole A moving in the same direction. This can have rather unpleasant consequences. For example, suppose there are two paired wormholes A at (1,1) and B at (3,1), and that Bessie the cow starts from position (2,1) moving in the +x direction. Bessie will enter wormhole B [at (3,1)], exit from A [at (1,1)], then enter B again, and so on, getting trapped in an infinite cycle!    | . . . .    | A > B .      Bessie will travel to B then    + . . . .      A then across to B again Farmer John knows the exact location of each wormhole on his farm. He knows that Bessie the cow always walks in the +x direction, although he does not remember where Bessie is currently located. Please help Farmer John count the number of distinct pairings of the wormholes such that Bessie could possibly get trapped in an infinite cycle if she starts from an unlucky position. FJ doesn't know which wormhole pairs with any other wormhole, so find all the possibilities (i.e., all the different ways that N wormholes could be paired such that Bessie can, in some way, get in a cycle). Note that a loop with a smaller number of wormholes might contribute a number of different sets of pairings to the total count as those wormholes that are not in the loop are paired in many different ways. PROGRAM NAME: wormhole INPUT FORMAT: Line 1: The number of wormholes, N. Lines 2..1+N: Each line contains two space-separated integers describing the (x,y) coordinates of a single wormhole. Each coordinate is in the range 0..1,000,000,000. SAMPLE INPUT (file wormhole.in): 4 0 0 1 0 1 1 0 1 INPUT DETAILS: There are 4 wormholes, forming the corners of a square. OUTPUT FORMAT: Line 1: The number of distinct pairings of wormholes such that Bessie could conceivably get stuck in a cycle walking from some starting point in the +x direction. SAMPLE OUTPUT (file wormhole.out): 2 OUTPUT DETAILS: If we number the wormholes 1..4 as we read them from the input, then if wormhole 1 pairs with wormhole 2 and wormhole 3 pairs with wormhole 4, Bessie can get stuck if she starts anywhere between (0,0) and (1,0) or between (0,1) and (1,1).    | . . . .    4 3 . . .      Bessie will travel to B then    1-2-.-.-.      A then across to B again Similarly, with the same starting points, Bessie can get stuck in a cycle if the pairings are 1-3 and 2-4 (if Bessie enters WH#3 and comes out at WH#1, she then walks to WH#2 which transports here to WH#4 which directs her towards WH#3 again for a cycle). Only the pairings 1-4 and 2-3 allow Bessie to walk in the +x direction from any point in the 2D plane with no danger of cycling.

--- January2017Silver/Wormholes/wormhole.cpp
+++ January2017Silver/Wormholes/wormhole.cpp
@@ -7,8 +7,13 @@ LANG: C++11
 #include <fstream>
 #include <string>
 #include <vector>
+#include <tuple>
 
 using namespace std;
+
+bool isLoop(const int y1, const int y2) {
+	return (y1 == y2);
+}
 
 void printMask(vector<int> &mask, const int level) {
 	for (int i = 0; i < level; i++)
@@ -43,27 +48,36 @@ void pickone(vector<int> &mask, const int level) {
 int picktwo(vector<int> &mask, const int level, int &count) {
 	//printMask(mask, level);
 	int n = (int)mask.size();
-	if (level < n) {
+	if (level < n/2) {
 		int i;
 		for (i = 0; i < n; ++i) {
-			if (mask[i] == 0) {
-				mask[i] = level + 1;
+			if (mask[i] < 0) {
+				mask[i] = level;
 				break;
 			}
 		}
 		for (int j = i + 1; j < n; ++j) {
-			if (mask[j] == 0) {
-				mask[j] = level + 2;
-				picktwo(mask, level + 2, count);
+			if (mask[j] < 0) {
+				mask[j] = i;
+				mask[i] = j;
+				/*
+				int x = mask[i];
+				mask[j] = mask[i];
+				mask[i] = x;
+				*/
+				picktwo(mask, level + 1, count);
 				//printMask(mask, level);
-				mask[j] = 0;
+				mask[j] = -1;
 			}
 		}
-		mask[i] = 0;
+		mask[i] = -1;
 	}
 	else {
 		for (int j = 0; j < n; ++j) {
-			cout << mask[j] << ' ';
+			if (j < mask[j]) {
+				cout << j << '-' << mask[j] << ' ';
+			}
+			// cout << mask[j] << ' ';
 		}
 		cout << endl;
 		count += 1;
@@ -76,8 +90,17 @@ int main() {
 	ofstream fout("wormhole.out");
 	ifstream fin("wormhole.in");
 	
-	int n = 8; 
-	vector<int> mask(n, 0);
+	int n;
+	fin >> n;
+	vector<tuple<int, int>> wormholes(n);
+	for (int y = 0; y < n; ++y) {
+		int wormholex, wormholey;
+		fin >> wormholex >> wormholey;
+		wormholes[y] = make_tuple(wormholex, wormholey);
+	}
+
+	int z = 6; 
+	vector<int> mask(z, -1);
 	int count = 0;
 
 	cout << picktwo(mask, 0, count) << endl; 
