d40871d6409364a0487be8a6194ca97338a30edd
CutieOwl <kml11@live.com>
2017-02-05 22:56:13 -0800

    Wormhole solved

---
+++ January2017Silver/January2017Silver/tupleCompare.cpp
@@ -0,0 +1,17 @@
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <tuple>
+
+using namespace std;
+
+template<int M, template<typename> class F = std::less>
+struct TupleCompare
+{
+	template<typename T>
+	bool operator()(T const &t1, T const &t2)
+	{
+		return F<typename tuple_element<M, T>::type>()(std::get<M>(t1), std::get<M>(t2));
+	}
+};
