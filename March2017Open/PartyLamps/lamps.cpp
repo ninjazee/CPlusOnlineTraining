7eed101670ace98412f03f12287b73b5de1dc8ad
CutieOwl <kml11@live.com>
2017-04-06 22:24:23 -0700

    party lamps press button function

--- March2017Open/PartyLamps/lamps.cpp
+++ March2017Open/PartyLamps/lamps.cpp
@@ -6,16 +6,31 @@ LANG: C++11
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <bitset>
 
 using namespace std;
 
-void followGraph (string comet) {
-	int retValue = 1;
-	for (char& c : comet) {
-		int diff = c - 'A' + 1;
-		retValue *= diff;
+void pressButton (vector<int> &states) {
+	int combinations = (int)states.size();
+	//vector<int> currentStates(combinations, 0);
+	vector<int> origStates(states);
+	fill(states.begin(), states.end(), 0);
+	// Calculate all possible states for this level.
+	for (int i = 0; i < combinations; ++i) {
+		if (origStates[i] != 0) { // a lamp is currently in this state
+			// button 1 is pressed: all lamps change state
+			states[i ^ 15] += 1; // 15 = 1111
+
+			// button 2 is pressed: odd lamps change state
+			states[i ^ 10] += 1; // 10 = 1010
+
+			// button 3 is pressed: even lamps change state
+			states[i ^ 5] += 1; // 5 = 0101
+
+			// button 3 is pressed: every 3rd lamp changes state
+			states[i ^ 9] += 1; // 9 = 1001
+		}
 	}
-	return retValue;
 }
 
 int main() {
@@ -38,7 +53,25 @@ int main() {
 		fin >> y;
 	}
 
-	int numTrack = 0;
+	vector<int> states(16, 0);
+	states[0] = 1;
+	for (int i = 0; i < c; ++i) {
+		pressButton(states);
+		cout << "Press " << i + 1 << endl;
+		for (int j = 0; j < (int)states.size(); ++j) {
+			cout << bitset<4>(j) << ": " << states[j] << endl;
+		}
+		cout << endl;
+	}
 
+	/*
+	for (int j = 0; j < (int)states.size(); ++j) {
+		if (states[j] != 0) { // some lamps are in this state
+			for (int a = 0; a < (int)onNums.size(); ++a) {
+
+			}
+		}
+	}
+	*/
 	return 0;
 }
