c7b19b75478c988c65164c6a0daf5fab7b144f3f
CutieOwl <kml11@live.com>
2017-04-16 14:53:19 -0700

    Controlling Companies program finish

--- March2017Open/ControllingCompanies/concom.cpp
+++ March2017Open/ControllingCompanies/concom.cpp
@@ -47,59 +47,60 @@ int main() {
 	ifstream fin("concom.in");
 	int n;
 	fin >> n;
-	vector<vector<int>> percents(n, vector<int>(n));
+	vector<vector<int>> percents(100, vector<int>(100));
 	for (int a = 0; a < n; ++a) {
 		int i, j, p;
 		fin >> i >> j >> p;
 		percents[i - 1][j - 1] = p;
 	}
 
-	for (int b = 0; b < n; ++b) {
+	for (int b = 0; b < 100; ++b) {
 		percents[b][b] = 100;
 	}
 
-	vector<vector<bool>> ownings(n, vector<bool>(n, false));
-	for (int i = 0; i < n; ++i) { // for every company owning another comapny
-		for (int j = 0; j < n; ++j) { // for every company owned
+	vector<vector<bool>> ownings(100, vector<bool>(100, false));
+	for (int i = 0; i < 100; ++i) { // for every company owning another comapny
+		for (int j = 0; j < 100; ++j) { // for every company owned
 			if (percents[i][j] > 50) { // this company directly owns this company
 				ownings[i][j] = true;
+				/*
 				if (i != j) {
 					fout << i + 1 << ' ' << j + 1 << endl;
 				}
+				*/
 			}
-			else { // this company does not directly own this company
-				int total = 0;
-				for (int k = 0; k < n; ++k) { // for every company that owns a part of this company
-					if (j != k && percents[i][k] > 50) {
-						total += percents[k][j];
+			
+		}
+	}
+	
+	bool bChange = true;
+	while (bChange) {
+		bChange = false;
+		for (int i = 0; i < 100; ++i) { // for every company owning another comapny
+			for (int j = 0; j < 100; ++j) { // for every company owned
+				if (!ownings[i][j]) { // this company does not directly own this company
+					int total = 0;
+					for (int k = 0; k < 100; ++k) { // for every company that owns a part of this company
+						if (j != k && ownings[i][k]) {
+							total += percents[k][j];
+						}
+					}
+					if (total > 50) {
+						ownings[i][j] = true;
+						bChange = true;
+						//fout << i + 1 << ' ' << j + 1 << endl;
 					}
 				}
-				if (total > 50) {
-					ownings[i][j] = true;
-					fout << i + 1 << ' ' << j + 1 << endl;
-				}
 			}
-			/*
-			if (i == j) {
-				ownings[i][j] = true;
-			}
-			else {
-				int total = 0;
-				controllingCompanies(percents, ownings, i, j, i, total);
-			}
-			*/
 		}
 	}
-	/*
+	
 	for (int x = 0; x < 100; ++x) {
 		for (int y = 0; y < 100; ++y) {
-			if (x != y) {
-				if (ownings[x][y]) {
-					cout << x + 1 << ' ' << y + 1 << endl;
-				}
+			if (ownings[x][y] && x != y) {
+				fout << x + 1 << ' ' << y + 1 << endl;
 			}
 		}
 	}
-	*/
 	return 0;
 }
