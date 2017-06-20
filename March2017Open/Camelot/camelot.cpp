cf5241a360362127d4b5faa50e1a6ef96498cc37
CutieOwl <kml11@live.com>
2017-06-19 22:05:55 -0700

    Camelot IOI 98 Centuries ago, King Arthur and the Knights of the Round Table used to meet every year on New Year's Day to celebrate their fellowship. In remembrance of these events, we consider a board game for one player, on which one chesspiece king and several knight pieces are placed on squares, no two knights on the same square.
    This example board is the standard 8x8 array of squares:
    
    The King can move to any adjacent square from  to  as long as it does not fall off the board:
    
    A Knight can jump from  to , as long as it does not fall off the board:
    
    During the play, the player can place more than one piece in the same square. The board squares are assumed big enough so that a piece is never an obstacle for any other piece to move freely.
    
    The player's goal is to move the pieces so as to gather them all in the same square - in the minimal number of moves. To achieve this, he must move the pieces as prescribed above. Additionally, whenever the king and one or more knights are placed in the same square, the player may choose to move the king and one of the knights together from that point on, as a single knight, up to the final gathering point. Moving the knight together with the king counts as a single move.
    
    Write a program to compute the minimum number of moves the player must perform to produce the gathering. The pieces can gather on any square, of course.
    
    PROGRAM NAME: camelot
    
    INPUT FORMAT
    
    Line 1: Two space-separated integers: R,C, the number of rows and columns on the board. There will be no more than 26 columns and no more than 30 rows.
    Line 2..end: The input file contains a sequence of space-separated letter/digit pairs, 1 or more per line. The first pair represents the board position of the king; subsequent pairs represent positions of knights. There might be 0 knights or the knights might fill the board. Rows are numbered starting at 1; columns are specified as upper case characters starting with `A'.
    SAMPLE INPUT (file camelot.in)
    
    8 8
    D 4
    A 3 A 8
    H 1 H 8
    The king is positioned at D4. There are four knights, positioned at A3, A8, H1, and H8.
    
    OUTPUT FORMAT
    
    A single line with the number of moves to aggregate the pieces.
    SAMPLE OUTPUT (file camelot.out)
    
    10
    SAMPLE OUTPUT ELABORATION
    
    They gather at B5.
    Knight 1: A3 - B5 (1 move)
    Knight 2: A8 - C7 - B5 (2 moves)
    Knight 3: H1 - G3 - F5 - D4 (picking up king) - B5 (4 moves)
    Knight 4: H8 - F7 - D6 - B5 (3 moves)
    1 + 2 + 4 + 3 = 10 moves.

--- March2017Open/Camelot/camelot.cpp
+++ March2017Open/Camelot/camelot.cpp
@@ -9,10 +9,15 @@ LANG: C++11
 #include <vector>
 #include <queue>
 #include <limits>
+#include <unordered_map>
+#include <algorithm>
+#include <cmath>
 
 using namespace std;
 
-int checkLocation(const int br, const int bc, const int currr, const int currc, const vector<tuple<int, int>> &knightpos, vector<vector<int>> &seen) {
+#define LARGE_NUMBER 9000000
+
+void checkLocation(const int br, const int bc, const int currr, const int currc, const vector<tuple<int, int>> &knightpos, vector<vector<int>> &seen) {
 	queue<tuple<int, int, int>> exam;
 	// Push node 0 in 
 	exam.push(make_tuple(currr, currc, 0));
@@ -28,13 +33,13 @@ int checkLocation(const int br, const int bc, const int currr, const int currc, 
 		// Add next nodes. 
 		if (r + 2 < br) { // we can go up
 			if (c + 1 < bc) { // we can go right
-				if (seen[r + 2][c + 1] == -1) { // we have not been here before
+				if (seen[r + 2][c + 1] == LARGE_NUMBER) { // we have not been here before
 					seen[r + 2][c + 1] = moves + 1;
 					exam.push(make_tuple(r + 2, c + 1, moves + 1));
 				}
 			}
 			if (c - 1 >= 0) { // we can go left
-				if (seen[r + 2][c - 1] == -1) { // we have not been here before
+				if (seen[r + 2][c - 1] == LARGE_NUMBER) { // we have not been here before
 					seen[r + 2][c - 1] = moves + 1;
 					exam.push(make_tuple(r + 2, c - 1, moves + 1));
 				}
@@ -42,13 +47,13 @@ int checkLocation(const int br, const int bc, const int currr, const int currc, 
 		}
 		if (r - 2 >= 0) { // we can go down
 			if (c + 1 < bc) { // we can go right
-				if (seen[r - 2][c + 1] == -1) { // we have not been here before
+				if (seen[r - 2][c + 1] == LARGE_NUMBER) { // we have not been here before
 					seen[r - 2][c + 1] = moves + 1;
 					exam.push(make_tuple(r - 2, c + 1, moves + 1));
 				}
 			}
 			if (c - 1 >= 0) { // we can go left
-				if (seen[r - 2][c - 1] == -1) { // we have not been here before
+				if (seen[r - 2][c - 1] == LARGE_NUMBER) { // we have not been here before
 					seen[r - 2][c - 1] = moves + 1;
 					exam.push(make_tuple(r - 2, c - 1, moves + 1));
 				}
@@ -56,13 +61,13 @@ int checkLocation(const int br, const int bc, const int currr, const int currc, 
 		}
 		if (c + 2 < bc) { // we can go right
 			if (r + 1 < br) { // we can go up
-				if (seen[r + 1][c + 2] == -1) { // we have not been here before
+				if (seen[r + 1][c + 2] == LARGE_NUMBER) { // we have not been here before
 					seen[r + 1][c + 2] = moves + 1;
 					exam.push(make_tuple(r + 1, c + 2, moves + 1));
 				}
 			}
 			if (r - 1 >= 0) { // we can go down
-				if (seen[r - 1][c + 2] == -1) { // we have not been here before
+				if (seen[r - 1][c + 2] == LARGE_NUMBER) { // we have not been here before
 					seen[r - 1][c + 2] = moves + 1;
 					exam.push(make_tuple(r - 1, c + 2, moves + 1));
 				}
@@ -70,26 +75,28 @@ int checkLocation(const int br, const int bc, const int currr, const int currc, 
 		}
 		if (c - 2 >= 0) { // we can go left
 			if (r + 1 < br) { // we can go up
-				if (seen[r + 1][c - 2] == -1) { // we have not been here before
+				if (seen[r + 1][c - 2] == LARGE_NUMBER) { // we have not been here before
 					seen[r + 1][c - 2] = moves + 1;
 					exam.push(make_tuple(r + 1, c - 2, moves + 1));
 				}
 			}
 			if (r - 1 >= 0) { // we can go down
-				if (seen[r - 1][c - 2] == -1) { // we have not been here before
+				if (seen[r - 1][c - 2] == LARGE_NUMBER) { // we have not been here before
 					seen[r - 1][c - 2] = moves + 1;
 					exam.push(make_tuple(r - 1, c - 2, moves + 1));
 				}
 			}
 		}
 	}
-
+	
+	/*
 	int retVal = 0;
 	for (int i = 0; i < (int)knightpos.size(); ++i) {
 		retVal += seen[get<0>(knightpos[i])][get<1>(knightpos[i])];
 	}
 	
 	return retVal;
+	*/
 }
 
 int main() {
@@ -98,9 +105,10 @@ int main() {
 	int r, c;
 	fin >> r >> c;
 	int kr;
-	char kc;
-	fin >> kc >> kr;
-	tuple<int, int> kingpos = make_tuple(kr - 1, kc - 'A');
+	char kingc;
+	fin >> kingc >> kr;
+	kr -= 1;
+	int kc = kingc - 'A';
 
 	vector<tuple<int, int>> knightpos;
 	while (!fin.eof()) {
@@ -113,19 +121,58 @@ int main() {
 		knightpos.push_back(make_tuple(r - 1, c - 'A'));
 	}
 
-	
-	int best = numeric_limits<int>::max();
-	for (int i = 0; i < r; ++i) {
-		for (int j = 0; j < c; ++j) {
-			vector<vector<int>> seen(r, vector<int>(c, -1));
-			int curr = checkLocation(r, c, i, j, knightpos, seen);
-			if (curr < best) {
-				best = curr;
-			}
+	int nKnights = (int)knightpos.size();
+	// create a record of distances (in moves) of knights to a specific spot
+	vector<vector<vector<int>>> distKnightRecords(r, vector<vector<int>>(c, vector<int>(nKnights, 0)));
+	vector<vector<vector<vector<int>>>> distRecords(r, vector<vector<vector<int>>>(c, vector<vector<int>>(r, vector<int>(c, 0))));
+	for (int i = 0; i < r; ++i) { // for every row number
+		for (int j = 0; j < c; ++j) { // for every column number
+			vector<vector<int>> seen(r, vector<int>(c, LARGE_NUMBER));
+			checkLocation(r, c, i, j, knightpos, seen);
+			for (int k = 0; k < nKnights; k++)
+				distKnightRecords[i][j][k] = seen[get<0>(knightpos[k])][get<1>(knightpos[k])];
+			distRecords[i][j] = seen;
 		}
 	}
 	
-	fout << best << endl;
-	
+	int best = numeric_limits<int>::max();
+	// try the distances of all of the meeting spots and all of the spots the king could go to
+	for (int i = 0; i < r; ++i) { // meeting spot row
+		for (int j = 0; j < c; ++j) { // meeting spot column
+			int sum = 0; 
+			for (int k = 0; k < nKnights; k++) // calculate the distance from all of the knights to the location
+				sum += distKnightRecords[i][j][k];
+
+			for (int k = 0; k < r; ++k) { // king meeting spot row
+				for (int l = 0; l < c; ++l) { // king meeting spot column
+					int sum1 = sum;
+					sum1 += distRecords[i][j][k][l];
+					// check the distance from the king meeting spot to the king
+					int rdist = abs(k - kr);
+					int cdist = abs(l - kc);
+					sum1 += max(rdist, cdist);
+
+
+					for (int p = 0; p < nKnights; p++) {
+						int sum2 = sum1;
+						// Knight p is redirected.
+						sum2 -= distKnightRecords[i][j][p];
+						sum2 += distKnightRecords[k][l][p];
+						if (sum2 < best) {
+							best = sum2;
+						}
+					}
+				}
+			}
+
+		}
+	}
+
+	if (nKnights == 0) {
+		fout << 0 << endl;
+	}
+	else {
+		fout << best << endl;
+	}
 	return 0;
 }
