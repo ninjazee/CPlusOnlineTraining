6fe8a3c10a4da105cb52f47be8c0dd131a1603bd
CutieOwl <kml11@live.com>
2017-07-17 22:05:39 -0700

    The Perfect Stall Hal Burch Farmer John completed his new barn just last week, complete with all the latest milking technology. Unfortunately, due to engineering problems, all the stalls in the new barn are different. For the first week, Farmer John randomly assigned cows to stalls, but it quickly became clear that any given cow was only willing to produce milk in certain stalls. For the last week, Farmer John has been collecting data on which cows are willing to produce milk in which stalls. A stall may be only assigned to one cow, and, of course, a cow may be only assigned to one stall. Given the preferences of the cows, compute the maximum number of milk-producing assignments of cows to stalls that is possible. PROGRAM NAME: stall4 INPUT FORMAT Line 1: One line with two integers, N (0 <= N <= 200) and M (0 <= M <= 200). N is the number of cows that Farmer John has and M is the number of stalls in the new barn. Line 2..N+1: N lines, each corresponding to a single cow. The first integer (Si) on the line is the number of stalls that the cow is willing to produce milk in (0 <= Si <= M). The subsequent Si integers on that line are the stalls in which that cow is willing to produce milk. The stall numbers will be integers in the range (1..M), and no stall will be listed twice for a given cow. SAMPLE INPUT (file stall4.in) 5 5 2 2 5 3 2 3 4 2 1 5 3 1 2 5 1 2 OUTPUT FORMAT A single line with a single integer, the maximum number of milk-producing stall assignments that can be made. SAMPLE OUTPUT (file stall4.out) 4

--- SummerTraining/PerfectStall/stall4.cpp
+++ SummerTraining/PerfectStall/stall4.cpp
@@ -9,8 +9,64 @@ LANG: C++11
 #include <queue>
 #include <tuple>
 #include <unordered_set>
+#include <unordered_map>
+#include <limits>
+#include <algorithm>
 
 using namespace std;
+
+int bfs(const int start, const int end, vector<unordered_map<int, int>> &adjList, vector<int> &prevNode) {
+	int m = (int)prevNode.size();
+	vector<int> capacityPath(m);
+	capacityPath[start] = numeric_limits<int>::max() / 10;
+	queue<int> exam;
+	exam.push(start); // add the starting place (the pond) to the queue
+	while (!exam.empty()) {
+		auto curr = exam.front();
+		exam.pop();
+		for (auto i : adjList[curr]) { // for all neighbors of current node
+			int nextNode = i.first;
+			int nextLength = i.second;
+			if ((nextLength > 0) && (prevNode[nextNode] == -1)) { // we have not seen this node before and there is available capacity
+				prevNode[nextNode] = curr; // change its parent to the current node
+				capacityPath[nextNode] = min(capacityPath[curr], nextLength);
+				if (nextNode != end) {
+					exam.push(nextNode);
+				}
+				else {
+					// we found the stream
+					return capacityPath[end];
+				}
+			}
+		}
+	}
+	return 0;
+}
+
+int edmondsKarp(const int start, const int end, vector<unordered_map<int, int>> &adjList) {
+	int m = (int)adjList.size();
+	int f = 0; // initial max flow is 0
+	bool foreverBool = true;
+	while (foreverBool) {
+		vector<int> prevNode(m, -1);
+		prevNode[start] = -2;
+		int search = bfs(start, end, adjList, prevNode);
+		if (search == 0) {
+			foreverBool = false;
+			break;
+		}
+		f += search;
+		// backtrack search and write flow;
+		int curNode = end;
+		while (curNode != start) {
+			int lastNode = prevNode[curNode];
+			adjList[curNode][lastNode] += search;
+			adjList[lastNode][curNode] -= search;
+			curNode = lastNode;
+		}
+	}
+	return f;
+}
 
 int main() {
 	ofstream fout("stall4.out");
@@ -18,7 +74,7 @@ int main() {
 	int n, m;
 	fin >> n >> m;
 	vector<vector<int>> cowStalls(n);
-	vector<vector<int>> stallCows(m);
+	//vector<vector<int>> stallCows(m);
 	for (int a = 0; a < n; ++a) {
 		int s;
 		fin >> s;
@@ -26,10 +82,11 @@ int main() {
 			int stall;
 			fin >> stall;
 			cowStalls[a].push_back(stall);
-			stallCows[stall - 1].push_back(a);
+			//stallCows[stall - 1].push_back(a);
 		}
 	}
 
+	/*
 	queue<tuple<int, unordered_set<int>>> exam;
 	for (int x = 0; x < cowStalls[0].size(); ++x) {
 		unordered_set<int> currLine;
@@ -58,7 +115,32 @@ int main() {
 			}
 		}
 	}
+	*/
 
-	cout << best << endl;
+	int numIntersections = m;
+	vector<unordered_map<int, int>> adjList(n + m + 2);
+	for (int x = 0; x < n; ++x) { // for every cow
+		// connect this cow to the "source" node
+		// make sure to make capacity 1 because only 1 cow can go to 1 stall
+		adjList[0][x + 1] = 1;
+		adjList[x + 1][0] = 0;
+	}
+	for (int y = 0; y < m; ++y) { // for every stall
+		// connect this stall to the "sink" node
+		// make sure to make capacity 1 because only 1 cow can go to 1 stall
+		adjList[y + n + 1][n + m + 1] = 1;
+		adjList[n + m + 1][y + n + 1] = 0;
+	}
+	for (int i = 0; i < n; ++i) { // for every cow
+		int numStalls = (int)cowStalls[i].size();
+		for (int j = 0; j < numStalls; ++j) { // for every stall this cow can go to
+			// link the cow to the stall
+			// make sure to make capacity 1 because only 1 cow can go to 1 stall
+			adjList[i + 1][cowStalls[i][j] + n] = 1;
+			adjList[cowStalls[i][j] + n][i + 1] = 0;
+		}
+	}
+
+	fout << edmondsKarp(0, n + m + 1, adjList) << endl;
 	return 0;
 }
