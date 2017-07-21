1eca10f90d116fe7f75c42625bd44a6d9d275774
CutieOwl <kml11@live.com>
2017-07-21 16:49:25 -0700

    JobProcessing finish!

--- SummerTraining/Project1/job.cpp
+++ SummerTraining/Project1/job.cpp
@@ -10,15 +10,6 @@ LANG: C++11
 
 using namespace std;
 
-int convert(string comet) {
-	int retValue = 1;
-	for (char& c : comet) {
-		int diff = c - 'A' + 1;
-		retValue *= diff;
-	}
-	return retValue;
-}
-
 int main() {
 	ofstream fout("job.out");
 	ifstream fin("job.in");
@@ -36,27 +27,62 @@ int main() {
 		fin >> b;
 		bMachines[y] = b;
 	}
-
-	multimap<int, int> endTimes; // a map of which one finishes fastest
+	
+	// a machines
+	multimap<int, int> aEndTimes; // a map of which one finishes fastest
 	for (int i = 0; i < m1; ++i) { // for every A machine
 		// insert
-		endTimes.insert(make_pair(aMachines[i], 0)); // end, start
+		aEndTimes.insert(make_pair(aMachines[i], 0)); // end, start
 	}
-
-	int time = 0;
+	int aTime = 0;
 	vector<tuple<int, int>> aJobs(n);
 	for (int i = 0; i < n; ++i) { // for every job
-		// assign it to the job that finishes first
-		auto curr = endTimes.begin();
+		// assign it to the machine that finishes first
+		auto curr = aEndTimes.begin();
 		int currend = curr->first;
 		int currstart = curr->second;
 		int jobLength = currend - currstart;
-		endTimes.erase(curr);
+		aEndTimes.erase(curr);
 		aJobs[i] = make_tuple(currstart, currend);
-		endTimes.insert(make_pair(currend + jobLength, currstart + jobLength));
-		time = currstart + jobLength;
+		aEndTimes.insert(make_pair(currend + jobLength, currstart + jobLength));
+		aTime = currstart + jobLength;
 	}
 
-	cout << time << endl;
+	// b machines
+	multimap<tuple<int, int>, int> bEndTimes; // a map of which one finishes fastest
+	for (int i = 0; i < m2; ++i) { // for every B machine
+		// insert
+		bEndTimes.insert(make_pair(make_tuple(bMachines[i], 0), i)); // end, start, machine #
+	}
+	int bTime = 0;
+	vector<tuple<int, int, int>> bJobs(n);
+	for (int i = 0; i < n; ++i) { // for every job
+		// assign it to the machine that finishes first
+		auto curr = bEndTimes.begin();
+		int currend = get<0>(curr->first);
+		int currstart = get<1>(curr->first);
+		int jobLength = currend - currstart;
+		int machNum = curr->second;
+		bEndTimes.erase(curr);
+		bJobs[i] = make_tuple(currstart, currend, machNum);
+		bEndTimes.insert(make_pair(make_tuple(currend + jobLength, currstart + jobLength), machNum));
+		bTime = currstart + jobLength;
+	}
+
+	int maxTotalTime = 0;
+	vector<int> bCoolDown(m2, 0);
+	for (int i = 0; i < n; ++i) { // for every job
+		// add the corresponding aJob and bJob on the opposite end of the spectrum
+		int currTotal = get<1>(aJobs[i]) + (get<1>(bJobs[n - i - 1]) - get<0>(bJobs[n - i - 1]));
+		if (bCoolDown[get<2>(bJobs[n - i - 1])] > get<1>(aJobs[i])) { // there is still some time before the machine finishes its last job
+			currTotal += bCoolDown[get<2>(bJobs[n - i - 1])] - get<1>(aJobs[i]); // add that time
+		}
+		bCoolDown[get<2>(bJobs[n - i - 1])] = currTotal;
+		if (currTotal > maxTotalTime) {
+			maxTotalTime = currTotal;
+		}
+	} 
+
+	fout << aTime << ' ' << maxTotalTime << endl;
 	return 0;
 }
