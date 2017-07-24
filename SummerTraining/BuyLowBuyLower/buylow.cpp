d27231ec6217f165ecf2468d44d04fa3bae00c3f
CutieOwl <kml11@live.com>
2017-07-23 18:22:01 -0700

    Buy Low, Buy Lower The advice to "buy low" is half the formula to success in the stock market. But to be considered a great investor you must also follow this problems' advice: "Buy low, buy lower" That is, each time you buy a stock, you must purchase more at a lower price than the previous time you bought it. The more times you buy at a lower price than before, the better! Your goal is to see how many times you can continue purchasing at ever lower prices. You will be given the daily selling prices of a stock over a period of time. You can choose to buy stock on any of the days. Each time you choose to buy, the price must be lower than the previous time you bought stock. Write a program which identifies which days you should buy stock in order to maximize the number of times you buy. By way of example, suppose on successive days stock is selling like this:  Day   1  2  3  4  5  6  7  8  9 10 11 12 Price 68 69 54 64 68 64 70 67 78 62 98 87 In the example above, the best investor (by this problem, anyway) can buy at most four times if they purchase at a lower price each time. One four day sequence (there might be others) of acceptable buys is: Day    2  5  6 10 Price 69 68 64 62 PROGRAM NAME: buylow INPUT FORMAT Line 1: N (1 <= N <= 5000), the number of days for which stock prices are available. Line 2..etc: A series of N positive space-separated integers (which may require more than one line of data) that tell the price for that day. The integers will fit into 32 bits quite nicely. SAMPLE INPUT (file buylow.in) 12 68 69 54 64 68 64 70 67 78 62 98 87 OUTPUT FORMAT Two integers on a single line: the length of the longest sequence of decreasing prices the number of sequences that have this length In counting the number of solutions, two potential solutions are considered the same (and would only count as one solution) if they repeat the same string of decreasing prices, that is, if they "look the same" when the successive prices are compared. Thus, two different sequence of "buy" days could produce the same string of decreasing prices and be counted as only a single solution. SAMPLE OUTPUT (file buylow.out) 4 2

--- SummerTraining/BuyLowBuyLower/buylow.cpp
+++ SummerTraining/BuyLowBuyLower/buylow.cpp
@@ -8,8 +8,74 @@ LANG: C++11
 #include <vector>
 #include <tuple>
 #include <unordered_set>
+#include <algorithm>
+#include <string>
 
 using namespace std;
+
+class BigNum {
+private:
+	vector<int> digits;
+public:
+	BigNum() { assign(0); }
+	BigNum(const int val) { assign(val); }
+	BigNum(BigNum const &bn);
+
+	void assign(const int n);
+	void add(const BigNum &second);
+	string ToString();
+};
+
+BigNum::BigNum(BigNum const &bn) {
+	for (int i = 0; i < (int)bn.digits.size(); ++i) {
+		digits.push_back(bn.digits[i]);
+	}
+}
+
+void BigNum::assign(const int n) {
+	int temp = n; // a storage
+	while (temp > 0) {
+		digits.push_back(temp % 10); // push this digit into digits
+		temp /= 10;
+	}
+}
+
+void BigNum::add(const BigNum &second) {
+	int carry = 0;
+	int sizeA = (int)digits.size(); // the number of digits in our current number
+	int sizeB = (int)second.digits.size(); // the number of digits in the second number
+	for (int i = sizeA; i < sizeB; i++)
+	{
+		digits.push_back(0);
+	}
+	int i = 0;
+	for (; i < sizeB; ++i) {
+		digits[i] = digits[i] + second.digits[i] + carry; // add the two digits
+		// deal with amount carried on
+		carry = digits[i] / 10; 
+		digits[i] %= 10; 
+	}
+	for (; i < sizeA; ++i)
+	{
+		digits[i] = digits[i] + carry; // add the two digits
+		// deal with amount carried on
+		carry = digits[i] / 10;
+		digits[i] %= 10;
+
+	}
+	if (carry != 0) {
+		digits.push_back(carry);
+	}
+	
+}
+
+string BigNum::ToString() {
+	string retstr;
+	for (int i = (int)digits.size() - 1; i >= 0; --i) {
+		retstr.push_back(digits[i] + '0');
+	}
+	return retstr;
+}
 
 int main() {
 	ofstream fout("buylow.out");
@@ -23,48 +89,48 @@ int main() {
 		prices[a] = price;
 	}
 
-	vector<tuple<int, long long>> totals(n); // number of times you buy, number of combinations
-	for (int i = n - 1; i >= 0; --i) { // for every sequence of numbers from day i on
+	vector<tuple<int, BigNum>> totals(n); // number of times you buy, number of combinations
+	for (int i = 0; i < n; ++i) { // for every sequence of numbers from day i back
 		int numTimes = 0;
-		long long numComb = 0;
-		unordered_set<int> seen; // unordered set of day numbers
-		for (int j = i + 1; j < n; ++j) { // for every number after i
-			if (prices[i] > prices[j]) { // if we can buy on this day (the price on this day is less than the price today)
+		BigNum numComb(1);
+		unordered_set<int> seen;
+		for (int j = 0; j < i; ++j) { // for every number after i
+			if (prices[i] < prices[j]) { // if we can buy on this day (the price on this day is more than the price today)
 				if (get<0>(totals[j]) > numTimes) { // if the number of times we can buy on this day is larger than our current best
-					if (seen.find(prices[j]) == seen.end()) { // if we have not already seen this value
-						numTimes = get<0>(totals[j]);
-						numComb = get<1>(totals[j]);
-						seen.insert(prices[j]);
-					}
+					numTimes = get<0>(totals[j]);
+					numComb = get<1>(totals[j]);
+					seen.clear();
+					seen.insert(prices[j]);
 				}
 				else if (get<0>(totals[j]) == numTimes) { // if the number of times we can buy on this day is equal to our current best
-					if (seen.find(prices[j]) == seen.end()) { // if we have not already seen this value
-						numComb += get<1>(totals[j]);
+					if (seen.find(prices[j]) == seen.end()) { // we haven't already seen the same number;
+						numComb.add(get<1>(totals[j]));
 						seen.insert(prices[j]);
 					}
 				}
 			}
 		}
 		get<0>(totals[i]) = numTimes + 1; // the total for this day is our best from the days before plus one more for today
-		if (numComb == 0) { // if we didn't find any days that worked
-			get<1>(totals[i]) = 1; // there is one for today
-		}
-		else { // we found some days that worked, use that number
-			get<1>(totals[i]) = numComb;
-		}
+		get<1>(totals[i]) = numComb;
 	}
 
 	int bestTimes = 0;
-	long long bestComb = 0;
-	for (int x = 0; x < n; ++x) { // for every tuple in our totals vector
+	BigNum bestComb(0);
+	unordered_set<int> seen;
+	for (int x = n - 1; x >= 0; --x) { // for every tuple in our totals vector (starting from the back to take care of duplicates
 		if (get<0>(totals[x]) > bestTimes) { // a new largest number of days
 			bestTimes = get<0>(totals[x]);
 			bestComb = get<1>(totals[x]);
+			seen.clear();
+			seen.insert(prices[x]);
 		}
 		else if (get<0>(totals[x]) == bestTimes) { // more combinations
-			bestComb += get<1>(totals[x]);
+			if (seen.find(prices[x]) == seen.end()) {
+				bestComb.add(get<1>(totals[x]));
+			}
 		}
 	}
-	fout << bestTimes << ' ' << bestComb << endl;
+	string bComb = bestComb.ToString();
+	fout << bestTimes << " " << bComb << endl;
 	return 0;
 }
