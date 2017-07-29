26280633663cbeb143bdd1c4372e77c7c05d50ca
CutieOwl <kml11@live.com>
2017-07-29 13:45:06 -0700

    Letter Game finish

--- SummerTraining/LetterGame/lgame.cpp
+++ SummerTraining/LetterGame/lgame.cpp
@@ -7,6 +7,8 @@ LANG: C++11
 #include <fstream>
 #include <string>
 #include <vector>
+#include <algorithm>
+#include <unordered_map>
 
 using namespace std;
 
@@ -65,6 +67,7 @@ int convert(const string &s) {
 			break;
 		case 'j':
 			total += 7;
+			break;
 		case 'k':
 			total += 6;
 			break;
@@ -105,11 +108,28 @@ int main() {
 	fin >> s;
 	int sSize = (int)s.length();
 	
+	vector<int> sTally(26, 0);
+	for (int i = 0; i < sSize; ++i) {
+		sTally[s[i] - 'a'] += 1;
+	}
+
 	vector<string> dictionary;
 	string ds;
 	din >> ds;
 	while (ds != ".") {
-		dictionary.push_back(ds);
+		bool work = true;
+		vector<int> tally(26, 0);
+		for (int x = 0; x < (int)ds.length(); ++x) {
+			tally[ds[x] - 'a'] += 1;
+		}
+		for (int y = 0; y < 26; ++y) {
+			if (tally[y] > sTally[y]) {
+				work = false;
+			}
+		}
+		if (work) {
+			dictionary.push_back(ds);
+		}
 		din >> ds;
 	}
 	int dictSize = (int)dictionary.size();
@@ -119,15 +139,19 @@ int main() {
 		dictionaryVals[a] = convert(dictionary[a]);
 	}
 
+	// deal with one word scores
 	int bestVal = 0;
 	vector<string> bestStrings;
+	vector<bool> canMake(dictSize, false);
+	vector<vector<int>> tallys(dictSize, vector<int>(26, 0));
 	for (int i = 0; i < dictSize; ++i) { // for every word in the dictionary
 		// check if we can make this word
 		int wordLength = (int)dictionary[i].length();
-		vector<bool> used(sSize, false);
 		bool wordMatched = true;
+		vector<bool> used(sSize, false);
 		for (int j = 0; j < wordLength; ++j) { // for every character in this word
 			bool matched = false;
+			tallys[i][dictionary[i][j] - 'a'] += 1;
 			for (int k = 0; k < sSize; ++k) { // for every character in s
 				// see if the character in the word matches the character in s
 				if (!used[k]) { // if we haven't already used this character
@@ -144,6 +168,7 @@ int main() {
 			}
 		}
 		if (wordMatched) { // we were able to make this word
+			canMake[i] = true;
 			if (dictionaryVals[i] > bestVal) { // this is better than the current best
 				bestVal = dictionaryVals[i];
 				bestStrings.clear();
@@ -155,9 +180,41 @@ int main() {
 		}
 	}
 
-	cout << bestVal << endl;
+	// deal with two word scores
+	for (int i = 0; i < dictSize; ++i) { // for every two words
+		for (int j = i + 1; j < dictSize; ++j) {
+			if (canMake[i] && canMake[j]) { // if you can make both of them
+				bool works = true;
+				int firstLength = (int)dictionary[i].length();
+				int secondLength = (int)dictionary[j].length();
+				if (firstLength + secondLength <= sSize) { // if the lengths are not too long
+					for (int a = 0; a < 26; ++a) {
+						if (tallys[i][a] + tallys[j][a] > sTally[a]) { // if there are more of this character than we have
+							works = false;
+							break;
+						}
+					}
+
+					if (works) { // they work
+						if (dictionaryVals[i] + dictionaryVals[j] > bestVal) { // this is better than the current best
+							bestVal = dictionaryVals[i] + dictionaryVals[j];
+							bestStrings.clear();
+							bestStrings.push_back(dictionary[i] + ' ' + dictionary[j]);
+						}
+						else if (dictionaryVals[i] + dictionaryVals[j] == bestVal) { // this is the same as the current best
+							bestStrings.push_back(dictionary[i] + ' ' + dictionary[j]);
+						}
+					}
+				}
+			}
+		}
+	}
+
+	sort(bestStrings.begin(), bestStrings.end());
+
+	fout << bestVal << endl;
 	for (int x = 0; x < (int)bestStrings.size(); ++x) {
-		cout << bestStrings[x] << endl;
+		fout << bestStrings[x] << endl;
 	}
 	return 0;
 }
