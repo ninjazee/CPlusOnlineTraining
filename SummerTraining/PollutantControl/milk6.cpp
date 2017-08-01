da3697b4b54374be99135f253eaf90f903d227c9
CutieOwl <kml11@live.com>
2017-07-31 17:46:04 -0700

    Pollutant Control Hal Burch It's your first day in Quality Control at Merry Milk Makers, and already there's been a catastrophe: a shipment of bad milk has been sent out. Unfortunately, you didn't discover this until the milk was already into your delivery system on its way to stores. You know which grocer that milk was destined for, but there may be multiple ways for the milk to get to that store. The delivery system is made up of a several warehouses, with trucks running from warehouse to warehouse moving milk. While the milk will be found quickly, it is important that it does not make it to the grocer, so you must shut down enough trucks to ensure that it is impossible for the milk to get to the grocer in question. Every route costs a certain amount to shut down. Find the minimum amount that must be spent to ensure the milk does not reach its destination, along with a set of trucks to shut down that achieves this goal at that cost. PROGRAM NAME: milk6 INPUT FORMAT Line 1: Two space separated integers, N and M. N (2 <= N <= 32) is the number of warehouses that Merry Milk Makers has, and M (0 <= M <= 1000) is the number of trucks routes run. Warehouse 1 is actually the productional facility, while warehouse N is the grocer to which which the bad milk was destined. Line 2..M+1: Truck routes: three space-separated integers, Si, Ei, and Ci. Si and Ei (1 <= Si,Ei <= N) correspond to the pickup warehouse and dropoff warehouse for the truck route. Ci (0 <= Ci <= 2,000,000) is the cost of shutting down the truck route. SAMPLE INPUT (file milk6.in) 4 5 1 3 100 3 2 50 2 4 60 1 2 40 2 3 80 OUTPUT FORMAT The first line of the output should be two integers, C and T. C is the minimum amount which must be spent in order to ensure the our milk never reaches its destination. T is the minimum number of truck routes that you plan to shut down in order to achive this goal. The next T lines sould contain a sorted list of the indexes of the truck routes that you suggest shutting down. If there are multiple sets of truck routes that achieve the goal at minimum cost, choose one that shuts down the minimum number of routes. If there are still multiple sets, choose the one whose initial routes have the smallest index. SAMPLE OUTPUT (file milk6.out) 60 1 3

---
+++ SummerTraining/PollutantControl/milk6.cpp
@@ -0,0 +1,133 @@
+/*
+ID: kml111
+PROG: milk6
+LANG: C++11
+*/
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <tuple>
+#include <unordered_map>
+#include <queue>
+#include <limits>
+#include <algorithm>
+
+using namespace std;
+
+int bfs(const int start, const int end, vector<unordered_map<int, long long>> &adjList, vector<int> &prevNode) {
+	int m = (int)prevNode.size();
+	vector<long long> capacityPath(m);
+	capacityPath[start] = numeric_limits<long long>::max() / 10;
+	queue<int> exam;
+	exam.push(start); // add the starting place (the productional facility) to the queue
+	while (!exam.empty()) {
+		auto curr = exam.front();
+		exam.pop();
+		for (auto i : adjList[curr]) { // for all neighbors of current node
+			int nextNode = i.first;
+			long long nextLength = i.second;
+			if ((nextLength > 0) && (prevNode[nextNode] == -1)) { // we have not seen this node before and there is available capacity
+				prevNode[nextNode] = curr; // change its parent to the current node
+				capacityPath[nextNode] = min(capacityPath[curr], nextLength);
+				if (nextNode != end) {
+					exam.push(nextNode);
+				}
+				else {
+					// we found the warehouse
+					return capacityPath[end];
+				}
+			}
+		}
+	}
+	return 0;
+}
+
+int edmondsKarp(const int start, const int end, vector<unordered_map<int, long long>> &adjList) {
+	int m = (int)adjList.size();
+	int f = 0; // initial max flow is 0
+	bool foreverBool = true;
+	while (foreverBool) {
+		vector<int> prevNode(m, -1);
+		prevNode[start] = -2;
+		int search = bfs(start, end, adjList, prevNode);
+		if (search == 0) {
+			foreverBool = false;
+			break;
+		}
+		f += search;
+		// backtrack search and write flow;
+		int curNode = end;
+		while (curNode != start) {
+			int lastNode = prevNode[curNode];
+			adjList[curNode][lastNode] += search;
+			adjList[lastNode][curNode] -= search;
+			curNode = lastNode;
+		}
+	}
+	return f;
+}
+
+int main() {
+	ofstream fout("milk6.out");
+	ifstream fin("milk6.in");
+	int n, m;
+	fin >> n >> m;
+	vector<unordered_map<int, long long>> adjList(n);
+	vector<vector<int>> adjMatrix(n, vector<int>(n));
+	for (int a = 0; a < m; ++a) {
+		int s, e, c;
+		fin >> s >> e >> c;
+		adjList[s - 1][e - 1] = c * 10000000 + a;
+		adjList[e - 1][s - 1] = 0;
+		adjMatrix[s - 1][e - 1] = a + 1;
+	}
+
+	int maxFlow = edmondsKarp(0, 3, adjList);
+
+	// create cut
+	vector<bool> black(n);
+	queue<int> exam;
+	exam.push(0);
+	while (!exam.empty()) {
+		auto curr = exam.front();
+		exam.pop();
+		black[curr] = true; // mark this node as black
+
+		for (auto i : adjList[curr]) { // for every node we can reach
+			// add it to the queue
+			int node = i.first;
+			if (!black[node]) { // we haven't already seen this node
+				if (i.second / 10000000 > 0) { // there is some flow
+					exam.push(node);
+				}
+			}
+		}
+	}
+
+	int cut = 0;
+	vector<int> routes;
+	for (int i = 0; i < n; ++i) { // for every node
+		if (black[i]) { // if this is a black node
+			for (int j = 0; j < n; ++j) { // for every node
+				if (!black[j]) { // if this is not a black node
+					auto found = adjList[j].find(i);
+					if (found != adjList[j].end() && found->second / 10000000 > 0) { // if there is a path here
+						// add the cost to the cut
+						cut += found->second / 10000000;
+						routes.push_back(adjMatrix[i][j]);
+					}
+				}
+			}
+		}
+	}
+
+	int numRoutes = (int)routes.size();
+	fout << cut << " " << numRoutes << endl;
+	fout << routes[0];
+	for (int x = 1; x < numRoutes; ++x) {
+		fout << " " << routes[x];
+	}
+	fout << endl;
+	return 0;
+}
