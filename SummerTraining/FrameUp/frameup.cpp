459bab763947c339860ca948a0cb0d345d4a31a2
CutieOwl <kml11@live.com>
2017-08-05 13:53:34 -0700

    Frame Up debug

--- SummerTraining/FrameUp/frameup.cpp
+++ SummerTraining/FrameUp/frameup.cpp
@@ -9,21 +9,59 @@ LANG: C++11
 #include <vector>
 #include <unordered_set>
 #include <limits>
+#include <algorithm>
 
 using namespace std;
 
-void findOrder(const vector<unordered_set<int>> &onTopList, const vector<int> &numArrows, vector<char> &order, const int node) {
+void findOrder(const vector<unordered_set<int>> &onTopList, const vector<int> &numArrows, const vector<vector<int>> &arrowNums, vector<string> &order, vector<vector<bool>> &seen, const vector<bool> &exists, const int combNum, int &numOfCombs, const int node) {
 	int leastArrowNum = numeric_limits<int>::max();
-	int leastArrowNode;
-	order.push_back(char(node + 'A'));
+	vector<int> leastArrowNode;
+	order[combNum].push_back(char(node + 'A'));
+	string currentComb(order[combNum]);
+	seen[combNum][node] = true;
+	vector<bool> newSeenVect(seen[combNum]);
 	for (auto curr : onTopList[node]) { // for everything the start node is on top of
 		if (numArrows[curr] < leastArrowNum) { // this has the least arrows
 			leastArrowNum = numArrows[curr];
-			leastArrowNode = curr;
+			leastArrowNode.clear();
+			leastArrowNode.push_back(curr);
+		}
+		else if (numArrows[curr] == leastArrowNum) {
+			leastArrowNode.push_back(curr);
 		}
 	}
+	
+	int otherArrowNum = (int)arrowNums[numArrows[node]].size();
+	vector<int> sameLevels;
+	if (otherArrowNum > 1) { // there are other frames with the same number of arrows
+		for (int i = 0; i < otherArrowNum; ++i) {
+			if (arrowNums[numArrows[node]][i] != node) {
+				if (onTopList[node].find(arrowNums[numArrows[node]][i]) == onTopList[node].end()) { // this frame is not on top of that frame
+					if (!seen[combNum][arrowNums[numArrows[node]][i]]) {
+						if (exists[arrowNums[numArrows[node]][i]]) {
+							sameLevels.push_back(arrowNums[numArrows[node]][i]);
+							order[combNum].push_back(char(arrowNums[numArrows[node]][i] + 'A'));
+							seen[combNum][arrowNums[numArrows[node]][i]] = true;
+						}
+					}
+				}
+			}
+		}
+	}
+
+
 	if (leastArrowNum < numeric_limits<int>::max()) { // we haven't reached the end node
-		findOrder(onTopList, numArrows, order, leastArrowNode);
+		for (int i = 0; i < (int)leastArrowNode.size(); ++i) {
+			if (i == 0) { // this is the first so we will make it part of the original strain
+				findOrder(onTopList, numArrows, arrowNums, order, seen, exists, combNum + i, numOfCombs, leastArrowNode[i]);
+			}
+			else { // this is an extra strain
+				order.push_back(currentComb);
+				numOfCombs += 1;
+				seen.push_back(newSeenVect);
+				findOrder(onTopList, numArrows, arrowNums, order, seen, exists, combNum + i, numOfCombs, leastArrowNode[i]);
+			}
+		}
 	}
 }
 
@@ -50,11 +88,13 @@ int main() {
 	}
 
 	// find the boundaries of the frames
-	vector<vector<int>> boundaries(numFrames, vector<int>(4, -1));
+	vector<vector<int>> boundaries(26, vector<int>(4, -1));
+	vector<bool> exists(26, false);
 	for (int i = 0; i < h; ++i) { // for every row
 		for (int j = 0; j < w; ++j) { // for every column
 			if (frameArray[i][j] != '.') { // if it is a letter
 				int letter = frameArray[i][j] - 'A';
+				exists[letter] = true;
 				if (boundaries[letter][0] == -1) { // we haven't ever seen this frame
 					// assume that this is the top right
 					boundaries[letter][0] = i;
@@ -89,31 +129,33 @@ int main() {
 		}
 	}
 
-	vector<unordered_set<int>> onTopList(numFrames, unordered_set<int>());
-	for (int i = 0; i < numFrames; ++i) {
-		char letter = i + 'A';
-		for (int r = boundaries[i][0]; r <= boundaries[i][2]; ++r) {
-			if (r == boundaries[i][0] || r == boundaries[i][2]) { // it's one of the top sides
-				for (int c = boundaries[i][1]; c <= boundaries[i][3]; ++c) {
-					if (frameArray[r][c] != letter) { // something is covering this frame up
-						int newLetter = frameArray[r][c] - 'A';
+	vector<unordered_set<int>> onTopList(26, unordered_set<int>());
+	for (int i = 0; i < 26; ++i) {
+		if (exists[i]) {
+			char letter = i + 'A';
+			for (int r = boundaries[i][0]; r <= boundaries[i][2]; ++r) {
+				if (r == boundaries[i][0] || r == boundaries[i][2]) { // it's one of the top sides
+					for (int c = boundaries[i][1]; c <= boundaries[i][3]; ++c) {
+						if (frameArray[r][c] != letter) { // something is covering this frame up
+							int newLetter = frameArray[r][c] - 'A';
+							if (onTopList[newLetter].find(i) == onTopList[newLetter].end()) {
+								onTopList[newLetter].insert(i);
+							}
+						}
+					}
+				}
+				else { // it's not a top side
+					if (frameArray[r][boundaries[i][1]] != letter) { // something is covering this frame up
+						int newLetter = frameArray[r][boundaries[i][1]] - 'A';
 						if (onTopList[newLetter].find(i) == onTopList[newLetter].end()) {
 							onTopList[newLetter].insert(i);
 						}
 					}
-				}
-			}
-			else { // it's not a top side
-				if (frameArray[r][boundaries[i][1]] != letter) { // something is covering this frame up
-					int newLetter = frameArray[r][boundaries[i][1]] - 'A';
-					if (onTopList[newLetter].find(i) == onTopList[newLetter].end()) {
-						onTopList[newLetter].insert(i);
-					}
-				}
-				if (frameArray[r][boundaries[i][3]] != letter) { // something is covering this frame up
-					int newLetter = frameArray[r][boundaries[i][3]] - 'A';
-					if (onTopList[newLetter].find(i) == onTopList[newLetter].end()) {
-						onTopList[newLetter].insert(i);
+					if (frameArray[r][boundaries[i][3]] != letter) { // something is covering this frame up
+						int newLetter = frameArray[r][boundaries[i][3]] - 'A';
+						if (onTopList[newLetter].find(i) == onTopList[newLetter].end()) {
+							onTopList[newLetter].insert(i);
+						}
 					}
 				}
 			}
@@ -121,28 +163,40 @@ int main() {
 	}
 
 	// find the number of arrows pointing to each frame
-	vector<int> numArrows(numFrames);
-	for (int i = 0; i < numFrames; ++i) {
+	vector<int> numArrows(26);
+	for (int i = 0; i < 26; ++i) {
 		for (auto it : onTopList[i]) {
 			numArrows[it] += 1;
 		}
 	}
+	vector<vector<int>> arrowFrames(26,vector<int>());
+	for (int j = 0; j < 26; ++j) {
+		arrowFrames[numArrows[j]].push_back(j);
+	}
 
 	// find the order of the frames
-	vector<char> order;
-	int start;
-	for (int i = 0; i < numFrames; ++i) { // find the start node
-		if (numArrows[i] == 0) { // we found the one on top
-			start = i;
-			break;
+	vector<string> order;
+	int numOfCombs = 0;
+	vector<vector<bool>> seen;
+	for (int i = 0; i < 26; ++i) { // find the start node
+		if (exists[i]) {
+			if (numArrows[i] == 0) { // we found one on top
+				seen.push_back(vector<bool>(26, false));
+				string nStr;
+				order.push_back(nStr);
+				numOfCombs += 1;
+				findOrder(onTopList, numArrows, arrowFrames, order, seen, exists, numOfCombs - 1, numOfCombs, i);
+			}
 		}
 	}
-	findOrder(onTopList, numArrows, order, start);
+	for (int i = 0; i < (int)order.size(); ++i) {
+		reverse(order[i].begin(), order[i].end());
+	}
+	sort(order.begin(), order.end());
 
 	// print
-	for (int i = (int)order.size() - 1; i >= 0; --i) {
-		cout << order[i];
+	for (int i = 0; i < (int)order.size(); ++i) {
+		cout << order[i] << endl;
 	}
-	cout << endl;
 	return 0;
 }
