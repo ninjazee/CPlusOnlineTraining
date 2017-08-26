bbd1878cd93a1dc42ecaf8864fbfb72a0f7bc8bc
CutieOwl <kml11@live.com>
2017-08-26 09:36:23 -0700

    Fencing the Cows Hal Burch Farmer John wishes to build a fence to contain his cows, but he's a bit short on cash right. Any fence he builds must contain all of the favorite grazing spots for his cows. Given the location of these spots, determine the length of the shortest fence which encloses them. PROGRAM NAME: fc INPUT FORMAT The first line of the input file contains one integer, N. N (0 <= N <= 10,000) is the number of grazing spots that Farmer john wishes to enclose. The next N line consists of two real numbers, Xi and Yi, corresponding to the location of the grazing spots in the plane (-1,000,000 <= Xi,Yi <= 1,000,000). The numbers will be in decimal format. SAMPLE INPUT (file fc.in) 4 4 8 4 12 5 9.3 7 8 OUTPUT FORMAT The output should consists of one real number, the length of fence required. The output should be accurate to two decimal places. SAMPLE OUTPUT (file fc.out) 12.00

---
+++ SummerTraining/FencingTheCows/fc.cpp
@@ -0,0 +1,68 @@
+/*
+ID: kml111
+PROG: fc
+LANG: C++11
+*/
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <tuple>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+
+template<int M, template<typename> class F = std::less>
+struct TupleCompare
+{
+	template<typename T>
+	bool operator()(T const &t1, T const &t2)
+	{
+		return F<typename tuple_element<M, T>::type>()(std::get<M>(t1), std::get<M>(t2));
+	}
+};
+
+int main() {
+	ofstream fout("fc.out");
+	ifstream fin("fc.in");
+	int n;
+	fin >> n;
+	vector<tuple<double, double>> grazingSpots(n);
+	for (int a = 0; a < n; ++a) {
+		double xi, yi;
+		fin >> xi >> yi;
+		grazingSpots[a] = make_tuple(xi, yi);
+	}
+
+	// calculate the angle each point makes with the x-axis (from 0 to 360)
+	double midx = 0;
+	double midy = 0;
+	for (int i = 0; i < n; ++i) {
+		double xi = get<0>(grazingSpots[i]);
+		double yi = get<1>(grazingSpots[i]);
+		midx += xi / n;
+		midy += yi / n;
+	}
+	vector<tuple<int, double>> anglePerm(n);
+	//vector<int> perm(n);
+	for (int i = 0; i < n; ++i) {
+		double xi = get<0>(grazingSpots[i]);
+		double yi = get<1>(grazingSpots[i]);
+		get<1>(anglePerm[i]) = atan2(yi - midy, xi - midx);
+		get<0>(anglePerm[i]) = i;
+	}
+
+	// sort points based on angle values
+	sort(anglePerm.begin(), anglePerm.end(), TupleCompare<1>());
+
+	// start making hull
+	vector<int> hull(n);
+	hull[0] = get<0>(anglePerm[0]);
+	hull[1] = get<0>(anglePerm[1]);
+	int hullpos = 2;
+	for (int p = 0; p < n; ++p) {
+		//while (hullpos > 1 && )
+	}
+	return 0;
+}
