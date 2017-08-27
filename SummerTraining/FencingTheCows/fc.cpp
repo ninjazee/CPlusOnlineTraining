1add73412fcb66c10986d193e7ea512f356dfda2
CutieOwl <kml11@live.com>
2017-08-27 09:59:29 -0700

    Fencing the Cows work

--- SummerTraining/FencingTheCows/fc.cpp
+++ SummerTraining/FencingTheCows/fc.cpp
@@ -25,6 +25,10 @@ struct TupleCompare
 	}
 };
 
+double zcrossprod(const double ux, const double uy, const double uz, const double vx, const double vy, const double vz) {
+	return (uy * vz - vy * uz) + (uz * vx - vz * ux) + (ux * vy - vx * uy);
+}
+
 int main() {
 	ofstream fout("fc.out");
 	ifstream fin("fc.in");
@@ -37,7 +41,7 @@ int main() {
 		grazingSpots[a] = make_tuple(xi, yi);
 	}
 
-	// calculate the angle each point makes with the x-axis (from 0 to 360)
+	// calculate the angle each point makes with a middle point (from 0 to 360)
 	double midx = 0;
 	double midy = 0;
 	for (int i = 0; i < n; ++i) {
@@ -63,8 +67,38 @@ int main() {
 	hull[0] = get<0>(anglePerm[0]);
 	hull[1] = get<0>(anglePerm[1]);
 	int hullpos = 2;
-	for (int p = 0; p < n; ++p) {
-		//while (hullpos > 1 && )
+	for (int p = 0; p < n - 1; ++p) {
+		// while the angle is larger than 180 degrees (concave)
+		while (hullpos > 1 && zcrossprod(get<0>(grazingSpots[hull[hullpos - 2] - hull[hullpos - 1]]), get<1>(grazingSpots[hull[hullpos - 2] - hull[hullpos - 1]]), 0, get<0>(grazingSpots[hull[hullpos - 1] - get<0>(anglePerm[p])]), get<1>(grazingSpots[hull[hullpos - 1] - get<0>(anglePerm[p])]), 0) < 0) {
+			hullpos -= 1;
+		}
+		hull[hullpos] = get<0>(anglePerm[p]);
+		hullpos += 1;
 	}
+
+	// add the last point
+	int p = get<0>(anglePerm[n - 1]);
+	while (hullpos > 1 && zcrossprod(get<0>(grazingSpots[hull[hullpos - 2] - hull[hullpos - 1]]), get<1>(grazingSpots[hull[hullpos - 2] - hull[hullpos - 1]]), 0, get<0>(grazingSpots[hull[hullpos - 1] - p]), get<1>(grazingSpots[hull[hullpos - 1] - p]), 0) < 0) {
+		hullpos -= 1;
+	}
+
+	int hullstart = 0;
+	bool flag = false;
+	do {
+		bool flag = false;
+		if (hullpos - hullstart >= 2 && zcrossprod(get<0>(grazingSpots[p - hull[hullpos - 1]]), get<1>(grazingSpots[p - hull[hullpos - 1]]), 0, get<0>(grazingSpots[hull[hullstart] - p]), get<0>(grazingSpots[hull[hullstart] - p]), 0)) {
+			p = hull[hullpos - 1];
+			hullpos -= 1;
+			flag = true;
+		}
+		if (hullpos - hullstart >= 2 && zcrossprod(get<0>(grazingSpots[hull[hullstart] - p]), get<1>(grazingSpots[hull[hullstart] - p]), 0, get<0>(grazingSpots[hull[hullstart + 1] - hull[hullstart]]), get<0>(grazingSpots[hull[hullstart + 1] - hull[hullstart]]), 0)) {
+			hullstart += 1;
+			flag = true;
+		}
+	} while (flag);
+	hull[hullpos] = p;
+	hullpos += 1;
+
+
 	return 0;
 }
